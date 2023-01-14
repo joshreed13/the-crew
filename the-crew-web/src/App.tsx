07d4d97d60dc81420ee78d8c967498a258f776c3
Josh Reed <reedjosh2626@gmail.com>
2023-01-14 13:55:43 -0600

    Add routing library

--- the-crew-web/src/App.tsx
+++ the-crew-web/src/App.tsx
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  useRouteError,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom"
 import './App.css';
+import ControlPanel from './ControlPanel';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={< Root />} errorElement={<ErrorPage />}>
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<IndexPage />} />
+        <Route path="controlpanel/" element={<ControlPanel />} />
+      </Route>
+    </Route >
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <RouterProvider router={router} />
+  );
+}
+
+function Root() {
+  return (
+    <>
+      <div id="sidebar">
+        <h1>🚀</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/controlpanel/`}>Control Panel</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
+      </div >
+      <div id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+function IndexPage() {
+  return <div>🌎✨🚀✨🪐</div>
+}
+
+function ErrorPage() {
+  const error: any = useRouteError();
+  console.error(error);
+
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
     </div>
   );
 }
---
+++ the-crew-web/src/ControlPanel.tsx
@@ -0,0 +1,3 @@
+export default function ControlPanel() {
+    return <div>Hello World!</div>;
+}
