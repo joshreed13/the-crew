5ce5d12bbe5ae435289a564db8084c5b3f57df59
Josh Reed <reedjosh2626@gmail.com>
2023-01-15 21:34:35 -0600

    Implement basic backend server

--- the-crew-backend/app.py
+++ the-crew-backend/app.py
@@ -1,9 +1,228 @@
 from flask import Flask, request, Response, jsonify
+import werkzeug.exceptions
+from model import *
+
 app = Flask(__name__)
 
+TASKID = 1
+STATE: RoundState = RoundState(
+    players=[
+        PlayerState("", []),
+        PlayerState("", []),
+        PlayerState("", []),
+        PlayerState("", [])
+    ],
+    objectives={},
+    tricks=[Trick([None, None, None, None])],
+)
 
-@app.route("/api/hand/<playerNum>", methods=['GET'])
-def set_hand(playerNum):
-    request.get_json()
-    # return jsonify(message=f"Hello Player {playerNum}")
+
+@app.route("/api/appstate", methods=['GET'])
+def get_state():
+    print(STATE)
+    selectedPlayer = 0
+
+    def toPlayer(player):
+        return {
+            "name": player.name,
+            "isCommander": Card("R", 4) in player.hand
+        }
+
+    def toCard(card):
+        return {
+            "suit": card.suit,
+            "value": card.value
+        } if card is not None else None
+
+    def toTask(id, task):
+        return {
+            "id": id,
+            "type": task.type,
+            "order": task.order,
+            "card": toCard(task.card),
+            "player": task.player,
+        }
+
+    tricks = [{
+        "turns": [{
+            "player": toPlayer(player),
+            "card": toCard(card),
+            "isLeader": False,
+            "isWinner": False,
+            "isNextToPlay": False,
+        } for player, card in zip(STATE.players, trick.turns)]
+    } for trick in STATE.tricks]
+
+    return jsonify({
+        "handPage": {
+            "heldCards": [toCard(card) for card in STATE.players[selectedPlayer].hand]
+        },
+        "objectivePage": {
+            "tasks": [toTask(id, task) for id, task in STATE.objectives.items()]
+        },
+        "tricksPage": {
+            "tricks": tricks
+        },
+        "controlPanel": {
+            "players": [{
+                "player": toPlayer(player),
+                "hand": [toCard(card) for card in player.hand],
+                "tasks": [toTask(id, task) for id, task in STATE.objectives.items() if task.player == i],
+            } for i, player in enumerate(STATE.players)],
+            "tricks": tricks
+        },
+    })
+
+
+@ app.route("/api/player/<int:playerNum>/name", methods=['PUT'])
+def setPlayerName(playerNum):
+    validateRequestIsJson(request)
+    playerNum = parsePlayerNum(playerNum)
+
+    name = request.get_json()["name"]
+    STATE.players[playerNum].name = name
+
     return Response("Success")
+
+
+@ app.route("/api/player/<int:playerNum>/hand", methods=['PUT'])
+def setPlayerHand(playerNum):
+    validateRequestIsJson(request)
+    playerNum = parsePlayerNum(playerNum)
+
+    cards = parseCards(request.get_json()["cards"])
+    STATE.players[playerNum].hand = cards
+
+    return Response("Success")
+
+
+@ app.route("/api/objective/add", methods=['POST'])
+def addObjective():
+    global TASKID
+    validateRequestIsJson(request)
+
+    objtype = request.get_json()["type"]
+    validateObjectiveType(objtype)
+    order = int(request.get_json()["order"])
+    validateObjectiveOrder(objtype, order)
+    card = parseCard(request.get_json()["card"])
+    playerNum = parsePlayerNum(request.get_json()["playerNum"])
+
+    STATE.objectives[TASKID] = Task(objtype, order, card, playerNum)
+    TASKID += 1
+
+    return Response("Success")
+
+
+@ app.route("/api/objective/<int:id>", methods=['DELETE'])
+def removeObjective(id):
+    validateTaskId(id)
+
+    del STATE.objectives[id]
+
+    return Response("Success")
+
+
+@ app.route("/api/objective/<int:id>/card", methods=['PUT'])
+def setObjectiveCard(id):
+    validateRequestIsJson(request)
+    validateTaskId(id)
+
+    card = parseCard(request.get_json()["card"])
+    STATE.objectives[id].card = card
+
+    return Response("Success")
+
+
+@ app.route("/api/objective/<int:id>/player", methods=['PUT'])
+def setObjectivePlayer(id):
+    validateRequestIsJson(request)
+    validateTaskId(id)
+
+    playerNum = parsePlayerNum(request.get_json()["playerNum"])
+    STATE.objectives[id].player = playerNum
+
+    return Response("Success")
+
+
+@ app.route("/api/trick/<int:trickIndex>/<int:turnIndex>/card", methods=['PUT'])
+def setTrickTurnCard(trickIndex, turnIndex):
+    validateRequestIsJson(request)
+    validateTrickIndex(trickIndex)
+    validateTurnIndex(trickIndex, turnIndex)
+
+    card = parseCard(request.get_json()["card"])
+    STATE.tricks[trickIndex].turns[turnIndex] = card
+
+    return Response("Success")
+
+
+def validateRequestIsJson(request):
+    if not request.is_json:
+        raise werkzeug.exceptions.UnsupportedMediaType("Expected JSON")
+
+
+def validatePlayerNum(playerNum):
+    if not (0 <= playerNum < 4):
+        raise werkzeug.exceptions.BadRequest("Bad player number")
+
+
+def validateCards(cards):
+    for card in cards:
+        validateCard(card)
+
+
+def validateCard(card: Card):
+    if card.suit == "R":
+        if not (0 < card.value <= 4):
+            raise werkzeug.exceptions.BadRequest("Bad card value")
+    elif card.suit in "BYMG":
+        if not (0 < card.value <= 9):
+            raise werkzeug.exceptions.BadRequest("Bad card value")
+    else:
+        raise werkzeug.exceptions.BadRequest("Bad card suit")
+
+
+def validateTrickIndex(trickIndex):
+    if not (0 <= trickIndex < len(STATE.tricks)):
+        raise werkzeug.exceptions.BadRequest("Bad trick index")
+
+
+def validateTurnIndex(trickIndex, turnIndex):
+    if not (0 <= turnIndex < len(STATE.tricks[trickIndex].turns)):
+        raise werkzeug.exceptions.BadRequest("Bad turn index")
+
+
+def validateObjectiveType(objtype):
+    if objtype not in {"absolute", "relative", "anytime", "last"}:
+        raise werkzeug.exceptions.BadRequest("Bad turn index")
+
+
+def validateObjectiveOrder(objtype, order):
+    currMax = max(obj.order for id, obj in STATE.objectives.items()
+                  if obj.type == objtype) if STATE.objectives else 0
+    if not (0 <= order <= currMax):
+        raise werkzeug.exceptions.BadRequest("Bad objective order")
+
+
+def validateTaskId(taskId):
+    if taskId not in STATE.objectives:
+        raise werkzeug.exceptions.BadRequest("Bad task id")
+
+
+def parsePlayerNum(playerNum):
+    playerNum = int(playerNum)
+    validatePlayerNum(playerNum)
+    return playerNum
+
+
+def parseCard(cardJson) -> Card:
+    card = Card(cardJson["suit"], cardJson["value"])
+    validateCard(card)
+    return card
+
+
+def parseCards(cardsJson) -> list[Card]:
+    cards = [parseCard(c) for c in cardsJson]
+    validateCards(cards)
+    return cards
--- the-crew-backend/model.py
+++ the-crew-backend/model.py
@@ -12,74 +12,29 @@ class Card:
 
 @dataclass_json
 @dataclass
-class Player:
+class PlayerState:
     name: str
-    isCommander: bool
-
-
-@dataclass_json
-@dataclass
-class Turn:
-    player: Player
-    card: Optional[Card]
-    isLeader: bool
-    isWinner: bool
-    isNextToPlay: bool
-
-
-@dataclass_json
-@dataclass
-class Trick:
-    turns: list[Turn]
+    hand: list[Card]
 
 
 @dataclass_json
 @dataclass
 class Task:
-    id: str
     type: str
     order: int
     card: Optional[Card]
-    player: Optional[Player]
+    player: Optional[int]
 
 
 @dataclass_json
 @dataclass
-class PlayerState:
-    player: Player
-    hand: list[Card]
-    tasks: list[Task]
+class Trick:
+    turns: list[Optional[Card]]
 
 
 @dataclass_json
 @dataclass
-class HandPageState:
-    heldCards: list[Card]
-
-
-@dataclass_json
-@dataclass
-class ObjectivePageState:
-    tasks: list[Task]
-
-
-@dataclass_json
-@dataclass
-class TricksPageState:
-    tricks: list[Trick]
-
-
-@dataclass_json
-@dataclass
-class ControlPanelState:
+class RoundState:
     players: list[PlayerState]
+    objectives: dict[int, Task]
     tricks: list[Trick]
-
-
-@dataclass_json
-@dataclass
-class AppState:
-    handPage: HandPageState
-    objectivePage: ObjectivePageState
-    tricksPage: TricksPageState
-    controlPanel: ControlPanelState
