80546d6403e16416bfa2259bf51afa01f8f34836
Josh Reed <reedjosh2626@gmail.com>
2023-01-19 17:15:41 -0600

    Hide data that doesn't belong to current player

--- the-crew-web/src/pages/Hand.tsx
+++ the-crew-web/src/pages/Hand.tsx
@@ -4,18 +4,23 @@ import { MultiCardPicker } from "../CardPicker";
 import { apiCall } from "../api";
 
 export default function HandPage({ data, selectedPlayer }: { data: HandPageData, selectedPlayer: number | undefined }) {
-    return (
-        <>
-            {data.heldCards.map((hand: Card[], i) => (
-                <>
-                    <div className="bordered">
-                        {hand.map((card) => (<CardView card={card} />))}
-                    </div>
-                    <MultiCardPicker callback={(cards: Card[]) => {
-                        apiCall(`/api/player/${i}/hand`, { cards: cards });
-                    }} />
-                </>
-            ))}
-        </>
-    );
+    if (selectedPlayer === undefined) {
+        return <div>Please select a player on the User page to see your hand</div>;
+    }
+    else if (selectedPlayer < data.heldCards.length) {
+        let hand = data.heldCards[selectedPlayer];
+        return (
+            <>
+                <div className="bordered">
+                    {hand.map((card) => (<CardView card={card} />))}
+                </div>
+                <MultiCardPicker callback={(cards: Card[]) => {
+                    apiCall(`/api/player/${selectedPlayer}/hand`, { cards: cards });
+                }} />
+            </>
+        );
+    }
+    else {
+        return <div>Loading...</div>;
+    }
 }
